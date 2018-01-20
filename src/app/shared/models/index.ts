e4b698ac2d30c72ce1bc0a8b6c6a6e99cfe31d29
Pubudu Ranasinghe <pubudutr@gmail.com>
2018-01-20 19:43:30 +0530

    question classes added

---
+++ src/app/shared/models/index.ts
@@ -0,0 +1,10 @@
+export * from './artifact-question';
+export * from './cascading-checkbox-question';
+export * from './checkbox-question';
+export * from './datetime-question';
+export * from './list-question';
+export * from './number-question';
+export * from './photo-question';
+export * from './question-group';
+export * from './text-question';
+
