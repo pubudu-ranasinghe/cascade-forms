c6dfea88a754a6b9e28e8ba876ec6d5c02ea18fa
Pubudu Ranasinghe <pubudutr@gmail.com>
2018-01-21 08:40:59 +0530

    chenged group to extend base question

--- src/app/shared/models/question-group.ts
+++ src/app/shared/models/question-group.ts
@@ -1,15 +1,17 @@
 import { BaseQuestion } from './base-question';
 
-export class QuestionGroup {
+export class QuestionGroup extends BaseQuestion<null> {
   questions: BaseQuestion<any>[];
   label: string;
   order: number;
+  controlType = 'group'
 
   constructor(options: {
     questions?: BaseQuestion<any>[],
     key?: string,
     order?: number
   }) {
+    super(options);
     this.questions = options.questions;
     this.label = options.key;
     this.order = options.order;
